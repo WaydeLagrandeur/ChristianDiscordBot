f75384afd75c2d0906f0ac0267bc539f9857240a
Wayde Lagrandeur <waydelagrandeur@gmail.com>
2021-11-04 17:26:08 -0600

    Added DB connection to track stats. Users can type !stats to view their total stats

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This is a bot that will make sure your servers stay holy and your users repent of their evil ways!
-To set up, download files, run NPM install, and create a "token" file, which is where you will put your bots authentication token
+To set up, download files, run NPM install, and create a "token" file, which is where you will put your bots authentication token.
 Have fun Prayer Warriors!
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const Sequelize = require('sequelize');
+const sqlite3 = require('sqlite3').verbose();
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
 const fs = require('fs');
@@ -6,17 +8,51 @@ const path = require('path');
 const prefix = '!';
 
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
-//Maybe add a way to track user statistics
+
+const sequelize = new Sequelize('database', 'user', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: 'database.sqlite',
+});
+
+const User = sequelize.define('User', {
+	name: {
+		type: Sequelize.STRING,
+    },
+    serverID: {
+        type: Sequelize.INTEGER,
+    },
+	sinCount: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0
+	},
+    praiseCount: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0
+	},
+    // thoughtsAndPrayersCount: {
+    //     type: Sequelize.INTEGER,
+    //     default: 0
+    // },
+    versesRead: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0
+    }
+});
+
 client.once('ready', () => {
     console.log('ChristianServerBot is online!');
+    User.sync();
+    //{ force: true }
 });
 
 client.on('message', message => {   
-    let badWords = ['fuck', 'shit', 'cunt', 'cock', 'dick', 'bitch', 'bastard', 'damn', 'dammit', 'ass', 'hell', 'pussy', 'whore', 'slut', 'piss', 'tit'];
-    let goodWords = ['frick', 'crap', 'good fellow', 'penis', 'penis', 'female dog', 'child of unwed parents', 'darn', 'darn it', 'bum', 'heck', 'vagina', 'promiscuous woman', 'promiscuous woman', 'pee', 'breast'];
+    let badWords = ['fuck', 'shit', 'cunt', 'cock', 'dick', 'bitch', 'bastard', 'damn', 'dammit', 'ass', 'hell', 'pussy', 'whore', 'slut', 'piss', 'tit', 'cum', 'fag', 'linux'];
+    let goodWords = ['frick', 'crap', 'good fellow', 'penis', 'penis', 'female dog', 'child of unwed parents', 'darn', 'darn it', 'bum', 'heck', 'vagina', 'promiscuous woman', 'promiscuous woman', 'pee', 'breast', 'semen', 'bundle of twigs', 'windows'];
     let bBadWords = false;
-    
-    for(let i = 0; i < badWords.length; i++) {        
+
+    for(let i = 0; i < badWords.length; i++) {       
         if(message.content.toLowerCase().includes(badWords[i]) && !message.author.bot) {
             message.content = message.content.toLowerCase().replaceAll(badWords[i], goodWords[i]);
             bBadWords = true;           
@@ -27,8 +63,53 @@ client.on('message', message => {
 
     if(bBadWords) {
         message.channel.send('That is a bad word ' + message.author.username + "! \nInstead, try saying \"" + newMessage + "\"");
+        UpdateCountDB(message.author.username, message.channel.id, 'sin');
     }
 });
+
+async function createUser(username, channel) {
+    
+    try {
+        const user = await User.create({
+            name: username,
+            serverID: channel,
+            versesRead: 0              
+        });
+                        
+    }
+    catch (error) {
+        console.log(error);
+    }
+      
+}
+async function UpdateCountDB(username, channel, count) {
+    
+    const user = await User.findOne({ where: { name: username, serverID: channel }});
+
+    if(user) {
+        if(count === 'sin') {
+            user.increment('sinCount');
+        }
+        if(count === 'praise') {
+            user.increment('praiseCount');
+        }
+        if(count === 'verse') {
+            user.increment('versesRead');
+        }
+    } else {
+        createUser(username, channel);
+        const newUser = await User.findOne({ where: { name: username, serverID: channel }});
+        if(count === 'sin') {
+            newUser.increment('sinCount');
+        }
+        if(count === 'praise') {
+            newUser.increment('praiseCount');
+        }
+        if(count === 'verse') {
+            newUser.increment('versesRead');
+        }
+    }
+}
 
 client.on('message', async message => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
@@ -36,29 +117,68 @@ client.on('message', async message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if(command === 'verse') {
-        const bibleVerse = await fetch('http://labs.bible.org/api/?passage=random&type=json').then(response => response.json());
 
-        const obj = JSON.stringify(bibleVerse, null, 2).toString().split('"');  
 
-        let passageLocation = obj[3] + " " + obj[7] + ":" + obj[11]; 
-        let passage = obj[15]; 
-        let sendOff = "This is the Word of the Lord. Praise be to God.";
+    if(command === 'stats') {
+        const user = await User.findOne({ where: { name: message.author.username, serverID: message.channel.id }});
 
-        const embed = new Discord.MessageEmbed().setTitle(passageLocation).setDescription(passage).setFooter(sendOff); 
+        if(user) {
+
+            let description = "Curse Count: " + user.sinCount +
+                            "\nPraise Count: " + user.praiseCount +
+                            "\nVerses Read: " + user.versesRead; 
+
+            const embed = new Discord.MessageEmbed().setTitle("Stats for " + user.name).setDescription(description); 
         
-        message.channel.send({embeds: [embed]}); 
-    }
-});
+            message.channel.send({embeds: [embed]});            
 
-client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-    
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+        } else {
+            createUser(message.author.username, message.channel.id);
+            const createdUser = await User.findOne({ where: { name: message.author.username, serverID: message.channel.id }});
+
+            let newDescription = "Curse Count: " + createdUser.sinCount +
+                            "\nPraise Count: " + createdUser.praiseCount +
+                            "\nVerses Read: " + createdUser.versesRead; 
+
+            const newEmbed = new Discord.MessageEmbed().setTitle("Stats for " + createdUser.name).setDescription(newDescription);
+            message.channel.send({embeds: [newEmbed]});
+        }      
+    }
+
+    if(command === 'leaderboard') {
+
+        // const sinLeaders = await User.findAll({ where: { serverID: message.channel.id } });
+        // message.channel.send('Here is a list of the biggest sinners:' + 
+        //                     '\n' + record here + ": " + record sin count + " sins");
+    }
 
     if(command === 'praise') {
         message.channel.send("Praise the Lord!");
+        UpdateCountDB(message.author.username, message.channel.id, 'praise');
+    }
+
+    // if(command === 'pray') {
+        
+    //     UpdateCountDB(message.author.username, message.channel.id, 'thoughtsAndPrayers');
+    // }
+
+    if(command === 'verse') {
+
+           try {
+                const bibleVerse = await fetch('http://labs.bible.org/api/?passage=random&type=json').then(response => response.json());
+                const obj = JSON.stringify(bibleVerse, null, 2).toString().split('"');  
+
+                let passageLocation = obj[3] + " " + obj[7] + ":" + obj[11]; 
+                let passage = obj[15]; 
+                let sendOff = "This is the Word of the Lord. Praise be to God.";
+
+                const embed = new Discord.MessageEmbed().setTitle(passageLocation).setDescription(passage).setFooter(sendOff);
+                message.channel.send({embeds: [embed]});
+            
+            } catch {
+                console.log(error);
+            }
+        UpdateCountDB(message.author.username, message.channel.id, 'verse'); 
     }
 });
 
@@ -66,3 +186,4 @@ fs.readFile('./token', 'utf8', function(err, data) {
     if(err) throw err;
     client.login(data);
 });
+
